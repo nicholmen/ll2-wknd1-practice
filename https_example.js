350f12bfd582911bb3f8b60961f5c61afbbe1da2
Thomas Nicholson <nicholmen@gmail.com>
2017-10-09 03:44:36 +0000

    changed callback to read the data

--- https_example.js
+++ https_example.js
@@ -8,12 +8,17 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
 
 https.request(options, callback).end();
 
-console.log("I've made the request!");
+console.log("I've made the request!");  
